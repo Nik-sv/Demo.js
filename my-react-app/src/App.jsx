20d6073628739a34dc0a7ae61ff4c1f5b84e65fc
Nikhil0938 <nt775139@gmail.com>
2024-10-21 15:28:13 +0530

    Your commit message

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {   // functional 
+  return (
+    <div>
+      <h1>Hello </h1>
+      <p>hii</p>
+  
+      </div> 
+    
+  )
+}
+
+export default App
